083a7a1a7a3dc8cca1c156c210c6e5472fdc4905
Hudzaifah-Hanafie <hanafiyaahmad@gmail.com>
2025-08-22 13:57:45 +0700

    menambahkan isi bubbleSort(), selectionSort(), dan insertionSort(), serta penggunaan method .sort()

--- script.js
+++ script.js
@@ -1,25 +1,58 @@
-const sortButton = document.getElementById('sort');
+const sortButton = document.getElementById("sort");
 
 const sortInputArray = (event) => {
-    event.preventDefault();
-    const inputValues = [...document.getElementsByClassName('values-dropdown')].map(dropdown => Number(dropdown.value));
-    const sortedValues = bubbleSort(inputValues);
-    updateUI(sortedValues);
+  event.preventDefault();
+  const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => Number(dropdown.value));
+  const sortedValues = inputValues.sort((a, b) => a - b);
+  updateUI(sortedValues);
 };
 
 const updateUI = (array = []) => {
-    array.forEach((num, i) => {
-        const outputValueNode  = document.getElementById(`output-value-${i}`);
-        outputValueNode.innerText = num;
-    });
+  array.forEach((num, i) => {
+    const outputValueNode = document.getElementById(`output-value-${i}`);
+    outputValueNode.innerText = num;
+  });
 };
 
 const bubbleSort = (array) => {
-    for (let i = 0; i < array.length - 1; i++) {
-        for (let j = 0; j < array.length - 1; j++) {
-            console.log(array, array[j], array[j+1]);
-        }
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
     }
+  }
+  return array;
 };
 
-sortButton.addEventListener('click', sortInputArray);
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+    const temp = array[i];
+    array[i] = array[minIndex];
+    array[minIndex] = temp;
+  }
+  return array;
+};
+
+const insertionSort = (array) => {
+  for (let i = 1; i < array.length; i++) { 
+    const currValue = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j] > currValue) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = currValue;
+  }
+    return array;
+};
+
+sortButton.addEventListener("click", sortInputArray);
